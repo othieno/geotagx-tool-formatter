7023557997fd5cee71055200f621c341af2a3ea9
Jeremy Othieno <j.othieno@gmail.com>
2017-03-23 22:56:04 +0100

    Implement the format_task_presenter_configuration function

--- src/task_presenter.py
+++ src/task_presenter.py
@@ -25,5 +25,51 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
-def format_task_presenter_configuration(task_presenter_configuration, validate_configuration=False):
+from geotagx_validator.helper import check_arg_type
+import geotagx_validator.task_presenter as validator
+
+def format_task_presenter_configuration(configuration, validate_configuration=False):
+    """Formats the specified task presenter configuration.
+
+    Args:
+        configuration (dict): A task presenter configuration to format.
+        validate_configuration (bool): If set to True, the specified configuration
+            will be validated before it's processed.
+
+    Raises:
+        TypeError: If the configuration argument is not a dictionary, or
+            validate_configuration is not a boolean.
+        ValueError: If the specified configuration is not a valid task presenter
+            configuration.
+    """
+    check_arg_type(format_task_presenter_configuration, "configuration", configuration, dict)
+    check_arg_type(format_task_presenter_configuration, "validate_configuration", validate_configuration, bool)
+
+    if validate_configuration:
+        valid, message = validator.is_task_presenter_configuration(configuration)
+        if not valid:
+            raise ValueError(message)
+
+    formatters = {
+        "language": format_task_presenter_language,
+        "subject": format_task_presenter_subject,
+        "questionnaire": format_task_presenter_questionnaire,
+    }
+    for key in configuration:
+        formatter = formatters.get(key)
+        if formatter:
+            configuration[key] = formatter(configuration[key], False)
+
+    return configuration
+
+
+def format_task_presenter_language(language, validate_language=True):
+    raise NotImplementedError
+
+
+def format_task_presenter_subject(subject, validate_subject=True):
+    raise NotImplementedError
+
+
+def format_task_presenter_questionnaire(questionnaire, validate_questionnaire=True):
     raise NotImplementedError

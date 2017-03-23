8e64a5c0534fdae6e0f515e32d2f803877f4cb05
Jeremy Othieno <j.othieno@gmail.com>
2017-03-23 15:57:24 +0100

    Implement the project configuration formatter functions

--- src/project.py
+++ src/project.py
@@ -61,12 +61,89 @@ def format_project_configuration(configuration, validate_configuration=True):
 
 
 def format_project_name(name, validate_name=True):
-    raise NotImplementedError
+    """Formats the specified project name.
+
+    Formatting a project name will simply remove any leading and trailing whitespace.
+
+    Args:
+        name (str): A project name to format.
+        validate_name (bool): If set to True, the specified project name will be
+            validated before it's processed.
+
+    Returns:
+        basestring: The formatted project name.
+
+    Raises:
+        TypeError: If the name argument is not a basestring instance, or
+            validate_name is not a boolean.
+        ValueError: If the specified name is not valid.
+    """
+    check_arg_type(format_project_name, "name", name, basestring)
+    check_arg_type(format_project_name, "validate_name", validate_name, bool)
+
+    if validate_name:
+        valid, message = validator.is_project_name(name)
+        if not valid:
+            raise ValueError(message)
+
+    return name.strip()
 
 
 def format_project_description(description, validate_description=True):
-    raise NotImplementedError
+    """Formats the specified project description.
+
+    Formatting a project description will simply remove any leading and trailing
+    whitespace.
+
+    Args:
+        description (str): A project description to format.
+        validate_description (bool): If set to True, the specified project
+            description will be validated before it's processed.
+
+    Returns:
+        basestring: The formatted project description.
+
+    Raises:
+        TypeError: If the description argument is not a basestring instance, or
+            validate_description is not a boolean.
+        ValueError: If the specified description is not valid.
+    """
+    check_arg_type(format_project_description, "description", description, basestring)
+    check_arg_type(format_project_description, "validate_description", validate_description, bool)
+
+    if validate_description:
+        valid, message = validator.is_project_description(description)
+        if not valid:
+            raise ValueError(message)
+
+    return description.strip()
 
 
 def format_project_repository(repository, validate_repository=True):
-    raise NotImplementedError
+    """Formats the specified project repository.
+
+    Formatting a project repository will simply remove any leading and trailing
+    whitespace.
+
+    Args:
+        repository (str): A project repository to format.
+        validate_repository (bool): If set to True, the specified project
+            repository will be validated before it's processed.
+
+    Returns:
+        basestring: The formatted project repository.
+
+    Raises:
+        TypeError: If the repository argument is not a basestring instance, or
+            validate_repository is not a boolean.
+        ValueError: If the specified repository is not valid.
+    """
+    check_arg_type(format_project_repository, "repository", repository, basestring)
+    check_arg_type(format_project_repository, "validate_repository", validate_repository, bool)
+
+    if validate_repository:
+        valid, message = validator.is_project_repository(repository)
+        if not valid:
+            raise ValueError(message)
+
+    return repository.strip()

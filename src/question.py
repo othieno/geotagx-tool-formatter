82435174dd660b1e35249b6ee1d4cd63fc3a8ede
Jeremy Othieno <j.othieno@gmail.com>
2017-03-29 17:10:37 +0200

    Implement the format_question_title and format_question_help functions

--- src/question.py
+++ src/question.py
@@ -62,26 +62,104 @@ def format_question(question, language, validate_configurations=True):
             raise ValueError(message)
 
     formatters = {
-        "title": lambda x: format_question_title(x, False),
-        "hint": lambda x: format_question_help(x, False),
-        "help": lambda x: format_question_help(x, False),
-        "input": lambda x: format_question_input(x, False),
+        "title": format_question_title,
+        "hint": format_question_help,
+        "help": format_question_help,
+        "input": format_question_input,
     }
     for key, value in question.iteritems():
         formatter = formatters.get(key)
         if formatter:
-            question[key] = formatter(value)
+            question[key] = formatter(value, language, False)
 
     return question
 
 
-def format_question_title(question_title, validate=True):
-    raise NotImplementedError
+def format_question_title(question_title, language, validate_configurations=True):
+    """Formats the specified question title.
+
+    This function will convert the question title into a normalized string. If the
+    title is already normalized, no action is performed and the original title is
+    returned.
+
+    Args:
+        question_title (basestring|dict): A question title configuration to format.
+        language (dict): A language configuration used to help format the title.
+        validate_configurations (bool): If set to True, the specified question title
+            and language configurations will be validated before they are processed.
+
+    Returns:
+        dict: The formatted question title.
+
+    Raises:
+        TypeError: If either the question_title or language argument is not a
+            dictionary, or validate_configurations is not a boolean.
+        ValueError: If either the question_title or language configuration is invalid.
+    """
+    check_arg_type(format_question_title, "question_title", question_title, (basestring, dict))
+    check_arg_type(format_question_title, "language", language, dict)
+    check_arg_type(format_question_title, "validate_configurations", validate_configurations, bool)
+
+    if validate_configurations:
+        from geotagx_validator.task_presenter import is_task_presenter_language
+        from geotagx_validator.question import is_question_title
+
+        valid, message = is_task_presenter_language(language)
+        if not valid:
+            raise ValueError(message)
+
+        valid, message = is_question_title(question_title)
+        if not valid:
+            raise ValueError(message)
+
+    from geotagx_validator.helper import is_normalized_string
+    from helper import normalize_string
+
+    return question_title if is_normalized_string(question_title) else normalize_string(question_title, language["default"])
 
 
-def format_question_help(question_help, validate=True):
-    raise NotImplementedError
+def format_question_help(question_help, language, validate_configurations=True):
+    """Formats the specified question help.
+
+    This function will convert the question help into a normalized string. If the
+    help is already normalized, no action is performed and the original help is
+    returned.
+
+    Args:
+        question_help (basestring|dict): A question help configuration to format.
+        language (dict): A language configuration used to help format the help.
+        validate_configurations (bool): If set to True, the specified question help
+            and language configurations will be validated before they are processed.
+
+    Returns:
+        dict: The formatted question help.
+
+    Raises:
+        TypeError: If either the question_help or language argument is not a
+            dictionary, or validate_configurations is not a boolean.
+        ValueError: If either the question_help or language configuration is invalid.
+    """
+    check_arg_type(format_question_help, "question_help", question_help, (basestring, dict))
+    check_arg_type(format_question_help, "language", language, dict)
+    check_arg_type(format_question_help, "validate_configurations", validate_configurations, bool)
+
+    if validate_configurations:
+        from geotagx_validator.task_presenter import is_task_presenter_language
+        from geotagx_validator.question import is_question_help
+
+        valid, message = is_task_presenter_language(language)
+        if not valid:
+            raise ValueError(message)
+
+        valid, message = is_question_help(question_help)
+        if not valid:
+            raise ValueError(message)
+
+    from geotagx_validator.helper import is_normalized_string
+    from helper import normalize_string
+
+    return question_help if is_normalized_string(question_help) else normalize_string(question_help, language["default"])
 
 
-def format_question_input(question_input, validate=True):
+def format_question_input(question_input, language, validate_configurations=True):
     raise NotImplementedError
